1533debd7d20c1d7d347f2020eb3b2cd107e5895
Ranger-HK <maxravindu99@gmail.com>
2024-06-21 11:40:40 +0530

    Updated

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,6 +3,7 @@ import { Button, Typography } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import { styleSheets } from "../Login/styles";
 import { withStyles } from "@mui/styles";
+import Alert from '@mui/material/Alert';
 
 
 class LoginPage extends Component {
@@ -23,6 +24,16 @@ class LoginPage extends Component {
         console.log("UserName : " + this.state.userName)
         console.log("Password : " + this.state.password)
         console.log(this.state.formData)
+
+        let formData = this.state.formData
+        if (formData.user_name === this.state.userName && formData.password === this.state.password) {
+            console.log("Credential Matched !")
+            //<Alert severity="success">Credential Matched !</Alert>
+        } else {
+            console.log("Credential Didn't Matched !")
+            //<Alert severity="error">Credential Didn't Matched !</Alert>
+
+        }
     }
 
     render() {
