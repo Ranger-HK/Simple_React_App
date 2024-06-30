12f1269e8c8b23943ed53b145a30e2af623dab1e
Ranger-HK <maxravindu99@gmail.com>
2024-06-30 20:46:22 +0530

    Updated

--- src/app/App.js
+++ src/app/App.js
@@ -17,7 +17,7 @@ function App() {
       <Route path="login" element={<LoginPage />} />
       <Route path="flex" element={<FlexBoxLayout/>} />
       <Route path="grid" element={<GridBoxLayout/>} />
-      <Route path="*" element={<NotFound />} />
+      {/*<Route path="*" element={<NotFound />} />*/}
     </Routes>
   );
 }
